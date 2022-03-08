86b6cf7ea367a2178fe2e3908e7c90897fd84c2e
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-03-08 11:01:03 -0600

    Update

--- HDT3/AlfPreg.R
+++ HDT3/AlfPreg.R
@@ -8,7 +8,7 @@ precio<-datos[,'SalePrice']
 q1<-data.frame(id, precio)
 ask1<-q1[order(-q1$precio),]
 ask1f<-head(ask1,n=5)
-
+View(ask1f)
 
 ggplot(data=ask1f, aes(x=reorder(id,-precio) , y=precio,fill=id)) +
   geom_bar(stat="identity")+
@@ -23,8 +23,9 @@ esti<-datos[,'HouseStyle']
 df<-data.frame(esti)
 ndf<-aggregate(df$esti, df, length)
 q2<-data.frame(ndf)
-q3<-head(q2)
+q3<-head(order(-q2),)
 
+View(q3)
 
 ggplot(data=q3, aes(x=reorder(esti, x) , y=x,fill=esti)) +
   geom_bar(stat="identity")+
@@ -39,7 +40,6 @@ esven<-datos[,'SaleType']
 de<-data.frame(esven)
 nde<-aggregate(de$esven, de, length)
 
-
 ggplot(data=nde, aes(x=reorder(esven, x) , y=x,fill=esven)) +
   geom_bar(stat="identity")+
   
@@ -52,6 +52,7 @@ ggplot(data=nde, aes(x=reorder(esven, x) , y=x,fill=esven)) +
 condi<-datos[, 'SaleCondition']
 dc<-data.frame(condi)
 ndc<-aggregate(dc$condi, dc, length)
+View(ndc)
 
 ggplot(data=ndc, aes(x=reorder(condi, x) , y=x,fill=condi)) +
   geom_bar(stat="identity")+
@@ -74,6 +75,37 @@ ggplot(data=ndm, aes(x=reorder(mesi, x) , y=x,fill=mesi)) +
 
 
 
+
+#ARBOL DE REGRESION
+library(rpart)
+library(caret)
+library(tree)
+library(rpart.plot)
+library(randomForest)
+
+train<-data[1:1460,]
+
+
+
+porciento <- 70/100
+
+datos <- data[,c("LotFrontage","LotArea","GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","X1stFlrSF","GarageYrBlt","GarageArea","YearRemodAdd", "SalePrice")]
+datos <- na.omit(datos)
+
+train <-na.omit(datos)
+datosFiltertree <- data[,c("LotFrontage","LotArea","GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","X1stFlrSF","GarageYrBlt","GarageArea","YearRemodAdd", "Estado")]
+
+
+
+set.seed(123)
+trainRowsNumber<-sample(1:nrow(datosFiltertree),porciento*nrow(datosFiltertree))
+train<-datosFiltertree[trainRowsNumber,]
+test<-datosFiltertree[-trainRowsNumber,]
+
+modeloRF1<-randomForest(train$Estado~.,train)
+prediccionRF1<-predict(modeloRF1,newdata = test)
+testCompleto<-test
+testCompleto$predRF<-prediccionR…
 
 
 
