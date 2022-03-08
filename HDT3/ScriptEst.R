55acd520dbd9b326d33e25749b1623edda0688c6
Estuardo <xavihernand7@hotmail.com>
2022-03-07 18:17:59 -0600

    Update

--- HDT3/ScriptEst.R
+++ HDT3/ScriptEst.R
@@ -22,3 +22,17 @@ pct <- round(table_zona/sum(table_zona)*100)
 lbls <- paste(lbls, pct)
 lbls <- paste(lbls,"%",sep="")
 pich<-pie(table_zona, labels = lbls, col=rainbow(length(lbls)), main="")
+
+
+#Pregunta 2
+#¿Qué tipo de carretera de acceso tienen la mayoría de las viviendas?
+calle<-data[,'Street']
+df1<-data.frame(calle)
+table_calle<-table(df1$calle)
+view(table_calle)
+
+lbls1 <- c("Gravel", "Paved")
+pct1 <- round(table_calle/sum(table_calle)*100)
+lbls1 <- paste(lbls1, pct1)
+lbls1 <- paste(lbls1,"%",sep="")
+pich1<-pie(table_calle, labels = lbls1, col=rainbow(length(lbls1)), main="")
